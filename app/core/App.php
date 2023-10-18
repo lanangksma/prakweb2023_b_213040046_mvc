9e7f0cfad908246ddfaa1f413e92118dc07267c9
lanangksma <kusumalanang1@gmail.com>
2023-10-18 17:29:09 +0700

    Membuat Aplikasi MVC dengan PHP #3 Routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,1 +1,17 @@
 <?php
+
+class App {
+    public function __construct() {
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/'); // rtrim() untuk menghapus tanda / di akhir url
+            $url = filter_var($url, FILTER_SANITIZE_URL); // filter_var() untuk membersihkan url dari karakter-karakter aneh
+            $url = explode('/', $url); // explode() untuk memecah url dengan delimiter / menjadi array
+            return $url;
+        }
+    }
+}
--- app/init.php
+++ app/init.php
@@ -1,1 +1,4 @@
 <?php
+
+require_once 'core/App.php';
+require_once 'core/Controller.php';
