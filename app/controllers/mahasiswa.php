99ee6b6a6f67655a9a65b1f21f0edbc10b100cd2
lanangksma <kusumalanang1@gmail.com>
2023-10-22 19:24:24 +0700

    Membuat Aplikasi MVC dengan PHP #7 Model

---
+++ app/controllers/mahasiswa.php
@@ -0,0 +1,10 @@
+<?php
+class mahasiswa extends Controller{
+    public function index() {
+        $data['judul'] = 'Daftar Mahasiswa';
+        $data['mhs'] = $this->model('mahasiswa_model')->getAllMahasiswa();
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('templates/footer');
+    }
+}
